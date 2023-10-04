ffd4209416553e19a38dd4cb791bca71e0a1405c
AlexeyD3 <111375168+AlexeyD3@users.noreply.github.com>
2023-10-04 22:25:53 +0300

    Update outputs.tf

--- 01/task2/outputs.tf
+++ 01/task2/outputs.tf
@@ -1,4 +1,3 @@
-output "external_ip" {
+output "vm_web_external_ip" {
   value = [yandex_compute_instance.platform.network_interface[0].nat_ip_address]
-  description = "vm external ip"
-}
+  description = "vm web external ip"
