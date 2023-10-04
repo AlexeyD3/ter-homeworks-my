73c5c1dd42eb48e868d946f2cb585155b17fe0cd
AlexeyD3 <111375168+AlexeyD3@users.noreply.github.com>
2023-10-04 22:26:52 +0300

    Create main.tf

---
+++ 01/task3/main.tf
@@ -0,0 +1,69 @@
+resource "yandex_vpc_network" "develop" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "develop" {
+  name           = var.vpc_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.develop.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+
+data "yandex_compute_image" "ubuntu" {
+  family = var.vm_web_image
+}
+resource "yandex_compute_instance" "platform" {
+  name        = var.vm_web_name
+  platform_id = var.vm_web_platform
+  resources {
+    cores         = var.vm_web_core_count
+    memory        = var.vm_web_memory
+    core_fraction = var.vm_web_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = var.vm_web_preemptible
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = var.vm_web_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_web_serial_port
+    ssh-keys           = "ubuntu:${file(var.vms_ssh_root_key)}"
+  }
+
+}
+
+resource "yandex_compute_instance" "database" {
+  name        = var.vm_db_name
+  platform_id = var.vm_db_platform
+  resources {
+    cores         = var.vm_db_core_count
+    memory        = var.vm_db_memory
+    core_fraction = var.vm_db_core_fraction
+  }
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = var.vm_db_preemptible
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = var.vm_db_nat
+  }
+
+  metadata = {
+    serial-port-enable = var.vm_db_serial_port
+    ssh-keys           = "ubuntu:${file(var.vms_ssh_root_key)}"
+  }
+
+}
