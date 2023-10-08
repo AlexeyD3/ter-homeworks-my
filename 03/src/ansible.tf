784e6f11ab3eab9e49d5df8ccb5ed4d1fd3bf8d9
wolinshtain <devops.alexey@yandex.ru>
2023-10-08 23:34:53 +0300

    07-terraform-02

--- 03/src/ansible.tf
+++ 03/src/ansible.tf
@@ -14,31 +14,19 @@ resource "local_file" "hosts_cfg" {
 }
 
 resource "null_resource" "web_hosts_provision" {
-#Ждем создания инстанса
 depends_on = [local_file.hosts_cfg]
-
-#Добавление ПРИВАТНОГО ssh ключа в ssh-agent
   provisioner "local-exec" {
     command = "cat ~/.ssh/id_ed25519 | ssh-add -"
   }
-
-#Костыль!!! Даем ВМ 60 сек на первый запуск. Лучше выполнить это через wait_for port 22 на стороне ansible
-# В случае использования cloud-init может потребоваться еще больше времени
-#  provisioner "local-exec" {
-#    command = "sleep 60"
-#  }
-
-#Запуск ansible-playbook
   provisioner "local-exec" {
     command  = "export ANSIBLE_HOST_KEY_CHECKING=False; ansible-playbook -i ${abspath(path.module)}/hosts.cfg ${abspath(path.module)}/test.yml"
-    on_failure = continue #Продолжить выполнение terraform pipeline в случае ошибок
+    on_failure = continue
     environment = { ANSIBLE_HOST_KEY_CHECKING = "False" }
-    #срабатывание триггера при изменении переменных
   }
     triggers = {
-      always_run         = "${timestamp()}" #всегда т.к. дата и время постоянно изменяются
-      playbook_src_hash  = file("${abspath(path.module)}/test.yml") # при изменении содержимого playbook файла
-      ssh_public_key     = var.vms_ssh.pub_key # при изменении переменной
+      always_run         = "${timestamp()}"
+      playbook_src_hash  = file("${abspath(path.module)}/test.yml")
+      ssh_public_key     = var.vms_ssh.pub_key
     }
 
 }
