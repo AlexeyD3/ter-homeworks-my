b2f160bab569d36785d200513baecef7804409a1
wolinshtain <devops.alexey@yandex.ru>
2023-10-09 20:25:47 +0300

    07-terraform-02

--- 04/src/variables.tf
+++ 04/src/variables.tf
@@ -1,16 +1,20 @@
 ###cloud vars
-variable "token" {
+###cloud vars
+variable "yc_token" {
   type        = string
+  sensitive   = true
   description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
 }
 
-variable "cloud_id" {
+variable "yc_cloud_id" {
   type        = string
+  sensitive   = true
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
 }
 
-variable "folder_id" {
+variable "yc_folder_id" {
   type        = string
+  sensitive   = true
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
 }
 
@@ -31,12 +35,20 @@ variable "vpc_name" {
   description = "VPC network&subnet name"
 }
 
-###common vars
+###ssh vars
 
-variable "vms_ssh_root_key" {
-  type        = string
-  default     = "your_ssh_ed25519_key"
-  description = "ssh-keygen -t ed25519"
+variable "vms_ssh" {
+
+  default = {
+    user               = "ubuntu"
+    serial-port-enable = 1
+    pub_key            = "~/.ssh/id_ed25519.pub"
+  }
+  type = object({
+    user               = string
+    serial-port-enable = number
+    pub_key            = string
+  })
 }
 
 ###example vm_web var
