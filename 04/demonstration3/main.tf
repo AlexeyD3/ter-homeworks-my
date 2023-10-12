be9838720eae3499e0a8b5cc5661c3c7de8ede77
wolinshtain <devops.alexey@yandex.ru>
2023-10-12 13:33:07 +0300

    task 6 vault

--- 04/demonstration3/main.tf
+++ 04/demonstration3/main.tf
@@ -8,15 +8,43 @@ provider "vault" {
         # checkov:skip=CKV_SECRET_6: education
 }
 
-data "vault_generic_secret" "vault_example"{
- path = "secret/example"
+resource "vault_generic_secret" "vault_write_ip" {
+  path    = "secret/example_ip"
+  data_json = <<EOT
+{
+  "ip": "192.168.2.15"
 }
-output "vault_example" {
- value = "${nonsensitive(data.vault_generic_secret.vault_example.data.1)}" #функция nonsensitive позволяет узнать значение sensitive данных
-} 
+EOT
+}
 
-#содержимое секретное. поглядеть можно через консоль
+data "vault_generic_secret" "vault_write_ip" {
+  depends_on = [vault_generic_secret.vault_write_ip]
+  path = "secret/example_ip"
+}
 
+output "vault_write_ip" {
+  depends_on = [data.vault_generic_secret.vault_write_ip]
+  value = "${nonsensitive(data.vault_generic_secret.vault_write_ip.data.ip)}"
+}
+
+resource "vault_generic_secret" "vault_write_pswd" {
+  path    = "secret/example"
+  data_json = <<EOT
+{
+  "mysql_user_password": "netology2"
+}
+EOT
+}
+
+data "vault_generic_secret" "vault_write_pswd" {
+  depends_on = [vault_generic_secret.vault_write_pswd]
+  path = "secret/example"
+}
+
+output "vault_write_pswd" {
+  depends_on = [data.vault_generic_secret.vault_write_pswd]
+  value = "${nonsensitive(data.vault_generic_secret.vault_write_pswd.data.mysql_user_password)}"
+}
 #> data.vault_generic_secret.vault_example # а содержимое data то скрыто!
 
 
