9cac37f6fb3f553625fa2f32789d00c60c0c875c
wolinshtain <devops.alexey@yandex.ru>
2023-10-13 21:20:56 +0300

    s3

--- 05/src/providers.tf
+++ 05/src/providers.tf
@@ -5,6 +5,18 @@ terraform {
     }
   }
   required_version = ">=0.13"
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket = "tfstate-netology-ad"
+    key = "terraform.tfstate"
+    region = "ru-central1"
+
+    skip_region_validation = true
+    skip_credentials_validation = true
+
+#    dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gm5cnlfgcnmeq4an1v/etn1oq33gdbq5bu4bk5t"
+#    dynamodb_table = "state-lock-table"
+  }
 }
 
 provider "yandex" {
