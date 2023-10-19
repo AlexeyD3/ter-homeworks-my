7f97975946cf0be99d4cafc5510a1529c1fcc9ff
wolinshtain <devops.alexey@yandex.ru>
2023-10-19 15:49:52 +0300

    tflint & checkov github-actions
    fix last warnings

--- 05/src/providers.tf
+++ 05/src/providers.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
       version = "0.100.0"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
     }
   }
   required_version = "=1.5.7"
